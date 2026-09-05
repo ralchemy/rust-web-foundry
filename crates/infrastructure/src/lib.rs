cd0fe92db33b163777793b8303a3ead38ed63ab1
oatiz <iamoatiz@gmail.com>
2026-09-05 21:12:45 +0800

    style: format infrastructure exports

--- crates/infrastructure/src/lib.rs
+++ crates/infrastructure/src/lib.rs
@@ -1,7 +1,7 @@
 mod mysql;
 mod outbound_http;
 
-pub use mysql::{MySqlReadinessProbe, MySqlTaskRepository, connect};
 #[cfg(feature = "reference-task")]
 pub use mysql::MIGRATOR;
+pub use mysql::{MySqlReadinessProbe, MySqlTaskRepository, connect};
 pub use outbound_http::HttpTaskPolicy;
