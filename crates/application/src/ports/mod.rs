48bff39ec5119e4e4c1acc3e1a5f217f7488b901
oatiz <iamoatiz@gmail.com>
2026-09-05 19:21:56 +0800

    feat: export TaskStarter port

--- crates/application/src/ports/mod.rs
+++ crates/application/src/ports/mod.rs
@@ -1,7 +1,9 @@
 mod readiness_probe;
 mod task_policy;
 mod task_repository;
+mod task_starter;
 
 pub use readiness_probe::ReadinessProbe;
 pub use task_policy::{TaskPolicy, TaskPolicyDecision, TaskPolicyInput};
 pub use task_repository::TaskRepository;
+pub use task_starter::TaskStarter;
