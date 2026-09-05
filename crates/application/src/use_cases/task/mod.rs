b53b8919de09a1631fa9b30be4a5169afa71bb50
oatiz <iamoatiz@gmail.com>
2026-09-05 18:52:36 +0800

    feat: export StartTask use case

--- crates/application/src/use_cases/task/mod.rs
+++ crates/application/src/use_cases/task/mod.rs
@@ -1,7 +1,9 @@
 mod create;
 mod get;
+mod start;
 mod view;
 
 pub use create::{CreateTask, CreateTaskCommand};
 pub use get::GetTask;
+pub use start::{StartTask, StartTaskCommand};
 pub use view::TaskView;
