04f20af0d025aa5f8edb2d9e6a3e48ee97879113
oatiz <iamoatiz@gmail.com>
2026-09-05 18:53:07 +0800

    feat: wire StartTask into HTTP state

--- crates/http/src/state.rs
+++ crates/http/src/state.rs
@@ -2,7 +2,7 @@ use application::ReadinessProbe;
 use axum::extract::FromRef;
 
 #[cfg(feature = "reference-task")]
-use application::{CreateTask, GetTask, TaskPolicy, TaskRepository};
+use application::{CreateTask, GetTask, StartTask, TaskPolicy, TaskRepository};
 
 #[derive(Clone)]
 pub(crate) struct HealthState<H>(pub(crate) H);
@@ -19,6 +19,7 @@ pub(crate) struct HttpState<P, R, H> {
 pub(crate) struct TaskState<P, R> {
     pub(crate) create: CreateTask<P, R>,
     pub(crate) get: GetTask<R>,
+    pub(crate) start: StartTask<R>,
 }
 
 #[cfg(feature = "reference-task")]
@@ -28,34 +29,19 @@ where
     R: TaskRepository,
     H: ReadinessProbe,
 {
-    pub(crate) fn new(create_task: CreateTask<P, R>, get_task: GetTask<R>, readiness: H) -> Self {
-        Self {
-            task: TaskState {
-                create: create_task,
-                get: get_task,
-            },
-            health: HealthState(readiness),
-        }
+    pub(crate) fn new(create_task: CreateTask<P, R>, get_task: GetTask<R>, start_task: StartTask<R>, readiness: H) -> Self {
+        Self { task: TaskState { create: create_task, get: get_task, start: start_task }, health: HealthState(readiness) }
     }
 }
 
 #[cfg(feature = "reference-task")]
 impl<P, R, H> FromRef<HttpState<P, R, H>> for TaskState<P, R>
-where
-    P: TaskPolicy,
-    R: TaskRepository,
-{
-    fn from_ref(state: &HttpState<P, R, H>) -> Self {
-        state.task.clone()
-    }
+where P: TaskPolicy, R: TaskRepository {
+    fn from_ref(state: &HttpState<P, R, H>) -> Self { state.task.clone() }
 }
 
 #[cfg(feature = "reference-task")]
 impl<P, R, H> FromRef<HttpState<P, R, H>> for HealthState<H>
-where
-    H: ReadinessProbe,
-{
-    fn from_ref(state: &HttpState<P, R, H>) -> Self {
-        state.health.clone()
-    }
+where H: ReadinessProbe {
+    fn from_ref(state: &HttpState<P, R, H>) -> Self { state.health.clone() }
 }
